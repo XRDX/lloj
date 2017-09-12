e71fb5976208b61ce8827bc909031df4873d4733
vic <305880887@qq.com>
2017-09-12 16:35:31 +0800

    add dbs

--- app.js
+++ app.js
@@ -4,26 +4,56 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var index = require('./routes/index');
+var multer = require('multer');
+var mongoose = require('mongoose');
+
+global.dbHandle = require('./databse/dbHandle');
+global.db = mongoose.connect("mongodb://localhost:27017/nodedb");
+
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express({
+    secret: 'secret',
+    cookie: {
+        maxAge: 1000*60*60;
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine("html", require("ejs").__express);
+app.set('view engine', 'html');
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
+app.use('/login', routes);
+app.use('/register', routes);
+app.use('/home', routes);
+app.use('/logout', routes);
+
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    delete req.session.error;
+    res.locals.message = "";
+    if(err){
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">' + err + '</div>';
+    }
+    next();
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ database/models.js
@@ -0,0 +1,6 @@
+module.exports = {
+    users: {
+        name: {type:String, required:true},
+        password:{type:String, required:true}
+    }
+};
