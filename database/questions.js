573e73d58fb91d02c25e65023597e5973d8dcdd8
vic <305880887@qq.com>
2017-09-14 17:35:08 +0800

    add question

--- database/questions.js
+++ database/questions.js
@@ -1,19 +1,19 @@
 module.exports = {
-    0: { id: 0, title: '编程练习I: 加法运算',
+    0: { title: '编程练习I: 加法运算',
         description: [
             "观察下面的输入及输出值，可以看出输出值是输入值的和，下面我们就来写一个函数add(x1,x2)，使得函数的返回值等于输入值的和。", 
-            "在JavaScript中，使用function来定义函数，使用return来返回结果。"
+            "在JavaScript中，使用function来定义函数，使用return来返回结果。",
+            "点击【提交】来执行你的代码"
         ],
-        default_code: "function add(x1, x2){\n    \/\/write your code here.\n    \n}\n",
-        tests:[
-        {x1: 2,  x2: 2,  y: 4,  ret:null},
-        {x1: 3,  x2: 2,  y: 5,  ret:null},
-        {x1: 12, x2: 12, y: 24, ret:null},
+        default_code: "function add(x1, x2){\n    var sum = x1 + x2;\n    return sum;\n}\n",
+        tests:[ {x1: 2,  x2: 2,  y: 4,  ret:null},
+            {x1: 3,  x2: 2,  y: 5,  ret:null},
+            {x1: 12, x2: 12, y: 24, ret:null},
             {x1: 54, x2: 21, y: 75, ret:null}
         ],
         function_name: "add"
     },
-    1: { id: 1, title: '编程练习II: 减法运算',
+    1: { title: '编程练习II: 减法运算',
         description: [
             "观察下面的输入及输出值，可以看出输出值是第一个数减去第二个数的值，下面我们就来写一个函数sub(x1,x2)，使得函数的返回值等于输入值的差。", 
             "不用担心数的大小问题"
@@ -27,7 +27,7 @@ module.exports = {
         ],
         function_name: "sub"            
     },
-    2: { id: 2, title: '编程练习III: 乘法运算',
+    2: { title: '编程练习III: 乘法运算',
         description: [
             "观察下面的输入及输出值，可以看出输出值是输入值的积，下面我们就来写一个函数mul(x1,x2)，使得函数的返回值等于输入值的乘积。", 
             "用*号作为乘号"
@@ -41,7 +41,7 @@ module.exports = {
         ],
         function_name: "mul"            
     },
-    3: { id: 3, title: '水仙花数',
+    3: { title: '水仙花数',
         description: [
             "如果一个三位数的百分位、十分位、个位的三次方之和等于这个三位数，那么这个数就称为水仙花数", 
             "写一个函数isNarcissusNumber(x)，来判断一个数是不是水仙花数"
@@ -55,7 +55,7 @@ module.exports = {
         ],
         function_name: "isNarcissusNumber"            
     },
-    4: { id: 4, title: '质数',
+    4: { title: '质数',
         description: [
             "如果一个数只能被1和自身整除，那么这个数就是一个质数", 
             "写一个函数isPrimeNumber(x)，来判断一个数是不是质数"
@@ -68,5 +68,20 @@ module.exports = {
             {x1: 13, y: true, ret:null}
         ],
         function_name: "isPrimeNumber"            
+    },
+    5: { title: '平均数',
+        description: [
+            "给出两个数字，计算平均值", 
+            "函数名称命名为average，带两个参数"
+        ],
+        default_code: "function average(x1, x2){\n    \/\/write your code here.\n    \n}\n",
+        tests:[
+            {x1: 4,  x2: 4, y: 4,  ret:null},
+            {x1: 7,  x2: 5, y: 6,  ret:null},
+            {x1: 9,  x2: 16, y: 12.5, ret:null},
+            {x1: 13, x2: 35, y: 24, ret:null}
+        ],
+        function_name: "average"            
     }
+
 }
